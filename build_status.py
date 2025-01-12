6d014972f8c427c93c70f2781106fe7d27ff6145
Maciej Olko <maciej.olko@gmail.com>
2025-01-12 18:57:41 +0100

    Variables rename

--- build_status.py
+++ build_status.py
@@ -22,15 +22,15 @@ def get_languages() -> Generator[tuple[str, str]]:
     defaults = config['defaults']
     for code, language in languages.items():
         language_code = code.lower().replace('_', '-')
-        switcher = language.get('in_prod', defaults['in_prod'])
-        yield language_code, switcher
+        in_switcher = language.get('in_prod', defaults['in_prod'])
+        yield language_code, in_switcher
 
 
 def main() -> None:
-    languages = {language: switcher for language, switcher in get_languages()}
+    languages = {language: in_switcher for language, in_switcher in get_languages()}
     print(languages)
     for code in ('en', 'pl', 'ar', 'zh-cn', 'id'):
-        print(f'{code}: {code in languages and languages[code]}')
+        print(f'{code}: {code in languages} {languages.get(code)}')
 
 
 if __name__ == '__main__':
