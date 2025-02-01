07618728144bb241e2927e6fc07059600db2ade4
Maciej Olko <maciej.olko@gmail.com>
2025-02-01 02:49:58 +0100

    Copy over only PO files to avoid warnings on extra files

--- build_warnings.py
+++ build_warnings.py
@@ -5,10 +5,13 @@ import sphinx.cmd.build
 
 
 def number(clones_dir: str, repo: str, language_code: str) -> int:
-    Path(clones_dir, f'cpython/Doc/locales/{language_code}').mkdir(parents=True)
-    Path(clones_dir, f'cpython/Doc/locales/{language_code}/LC_MESSAGES').symlink_to(
-        Path(clones_dir, repo), True
-    )
+    locale_dir = Path(clones_dir, f'cpython/Doc/locales/{language_code}/LC_MESSAGES')
+    locale_dir.mkdir(parents=True)
+    for po_file in Path(clones_dir, repo).rglob('*.po'):
+        relative_path = po_file.relative_to(Path(clones_dir, repo))
+        target_file = locale_dir / relative_path
+        target_file.parent.mkdir(parents=True, exist_ok=True)
+        copyfile(po_file, target_file)
     sphinx.cmd.build.main(
         (
             '--builder',
