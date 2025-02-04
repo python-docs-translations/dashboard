856763c562e5ea2266dd4911afec70fd9b9bea57
Maciej Olko <maciej.olko@gmail.com>
2025-02-04 23:39:19 +0100

    Retry Plausible stats using backoff

--- generate.py
+++ generate.py
@@ -6,6 +6,7 @@
 #     "jinja2",
 #     "requests",
 #     "docutils",
+#     "backoff",
 # ]
 # ///
 import subprocess
--- visitors.py
+++ visitors.py
@@ -1,17 +1,19 @@
 import csv
 import io
-import urllib
+import urllib.parse
 import zipfile
 
+import backoff
 from requests import Session
 
 
+@backoff.on_exception(backoff.expo, zipfile.BadZipFile, max_tries=5)
 def get_number_of_visitors(language: str, requests: Session) -> int:
     params = urllib.parse.urlencode(
         {'filters': f'[["contains","event:page",["/{language}/"]]]', 'period': 'all'}
     )
     r = requests.get(
-        f'https://plausible.io/docs.python.org/export?{params}', timeout=20
+        f'https://plausible.io/docs.python.org/export?{params}', timeout=40
     )
     with (
         zipfile.ZipFile(io.BytesIO(r.content), 'r') as z,
