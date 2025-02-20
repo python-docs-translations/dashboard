e1c9eaf22a8d5909f8b71002271c92d06d935ecb
Maciej Olko <maciej.olko@gmail.com>
2025-02-20 11:28:37 +0100

    Revert "Remove visitors tracking (#69)"
    This reverts commit db8166094b1c3cc4a633a8ba27fb730fbccb33aa.

---
+++ visitors.py
@@ -0,0 +1,29 @@
+import csv
+import io
+import urllib.parse
+import zipfile
+from logging import info
+
+from urllib3 import PoolManager, Retry
+
+
+def get_number_of_visitors(language: str, http: PoolManager) -> int:
+    params = urllib.parse.urlencode(
+        {'filters': f'[["contains","event:page",["/{language}/"]]]', 'period': 'all'}
+    )
+    response = http.request(
+        'GET',
+        f'https://plausible.io/docs.python.org/export?{params}',
+        retries=Retry(status_forcelist=(404, 500, 502)),
+    )
+    info(f'visitors {response.status=} ({language=})')
+    with (
+        zipfile.ZipFile(io.BytesIO(response.data), 'r') as z,
+        z.open('visitors.csv') as csv_file,
+    ):
+        csv_reader = csv.DictReader(io.TextIOWrapper(csv_file))
+        return sum(int(row['visitors']) for row in csv_reader)
+
+
+if __name__ == '__main__':
+    print(get_number_of_visitors('pl', PoolManager()))
