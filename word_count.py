1ed065b3a268098f01786d1916ba05db8b534a3f
Maciej Olko <maciej.olko@gmail.com>
2025-07-16 00:23:34 +0200

    Add string count information
    String count is a popular metric, used e.g. by Weblate tiers

--- word_count.py
+++ word_count.py
@@ -1,20 +1,26 @@
-import os
+from pathlib import Path
+
 from polib import pofile
 
 
-def _count_words(pot) -> int:
+def _count(pot) -> tuple[int, int]:
     pot = pofile(pot)
+    string_count = 0
     word_count = 0
     for entry in pot:
+        string_count += 1
         word_count += len(entry.msgid.split())
-    return word_count
+    return string_count, word_count
 
 
-def get_word_count(dir) -> int:
+def get_counts(dir: Path) -> tuple[int, int]:
+    total_string_count = 0
     total_word_count = 0
-    for root, dirs, files in os.walk(dir):
+    for root, dirs, files in dir.walk():
         for file in files:
             if file.endswith('.pot'):
-                pot = os.path.join(root, file)
-                total_word_count += _count_words(pot)
-    return total_word_count
+                pot = root.joinpath(root, file)
+                strings, words = _count(pot)
+                total_string_count += strings
+                total_word_count += words
+    return total_string_count, total_word_count
