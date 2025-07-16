3bfafd78e619678a17bde6426271770df28e7336
Maciej Olko <maciej.olko@gmail.com>
2025-07-16 09:38:14 +0200

    Fix

--- counts.py
+++ counts.py
@@ -5,12 +5,10 @@ from polib import pofile
 
 def _count(pot) -> tuple[int, int]:
     pot = pofile(pot)
-    string_count = 0
     word_count = 0
     for entry in pot:
-        string_count += 1
         word_count += len(entry.msgid.split())
-    return string_count, word_count
+    return len(pot), word_count
 
 
 def get_counts(dir: Path) -> tuple[int, int]:
@@ -20,7 +18,13 @@ def get_counts(dir: Path) -> tuple[int, int]:
         for file in files:
             if file.endswith('.pot'):
                 pot = root.joinpath(root, file)
-                strings, words = _count(pot)
+                strings, words = _count(pot.read_text())
                 total_string_count += strings
                 total_word_count += words
     return total_string_count, total_word_count
+
+
+if __name__ == '__main__':
+    print(
+        get_counts(Path(__file__).parent.joinpath('clones/cpython/Doc/build/gettext'))
+    )
