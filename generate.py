b58dc914238eb3af890aeb633a66daaaec41e843
Stan Ulbrych <stanulbrych@gmail.com>
2025-08-30 18:10:31 +0100

    Commit

--- generate.py
+++ generate.py
@@ -96,7 +96,7 @@ if __name__ == '__main__':
     logging.info(f'starting at {generation_time}')
     Path('build').mkdir(parents=True, exist_ok=True)
 
-    from data import completion_progress
+    completion_progress = list(get_completion_progress())
 
     env = Environment(loader=FileSystemLoader('templates'))
     index = env.get_template('index.html.jinja').render(
