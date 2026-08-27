5891811b83a3ae0dbf73f41427132733b151901a
Maciej Olko <maciej.olko@gmail.com>
2026-08-27 23:14:21 +0200

    Revert test changes

--- tests/test_index.py
+++ tests/test_index.py
@@ -59,25 +59,9 @@ class testIndex(unittest.TestCase):
 
         index = env.get_template('index.html.jinja').render(
             completion_progress=[language_project_data, higher_completion_score],
-            python_version='3.15',
-            release_cycle_status='prerelease',
             generation_time=datetime.now(),
             duration=100,
         )
-        self.assertIn(
-            '<option value="completion-score" selected>Completion score</option>', index
-        )
-        self.assertIn('<option value="completion">Overall completion</option>', index)
-        self.assertIn('<option value="core-completion">Core completion</option>', index)
-        self.assertIn('<option value="progress">Recent changes</option>', index)
-        self.assertNotIn('<option value="audience">', index)
-        self.assertIn('data-completion-score="100"', index)
-        self.assertIn('data-core-completion="100"', index)
-        self.assertIn('data-progress="2"', index)
-        self.assertIn("document.querySelector('#languageContainer > .row')", index)
-        self.assertIn("const sortStorageKey = 'dashboard-sort-by'", index)
-        self.assertIn('localStorage.getItem(sortStorageKey)', index)
-        self.assertIn('localStorage.setItem(sortStorageKey, this.value)', index)
 
         self.assertLess(index.index('German'), index.index('Polish'))
 
